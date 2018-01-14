db571a0e1fd93725012dcc835e7697d17060c51c
Zack Kollar <rallokkcaz@gmail.com>
2018-01-14 07:51:54 -0800

    init commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from djsetup.version import Version
+
+
+setuptools.setup(name='djsetup',
+                 version=Version('0.0.1').number,
+                 description='Django Setup Utility',
+                 long_description=open('README.md').read().strip(),
+                 author='Zack Kollar',
+                 author_email='zackkollar@gmail.com',
+                 url='http://path-to-my-packagename',
+                 py_modules=['djsetup'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='django boilerplate setup',
+                 classifiers=['Packages', 'Boilerplate'])
---
+++ tests/unit/test_version.py
@@ -0,0 +1,15 @@
+from djsetup.version import Version
+from tests import *
+from tests.helpers import *
+
+
+class TestVersion(unittest.TestCase):
+
+    def test_set_version(self):
+        ver = Version("0.0.1")
+        self.assertEqual(ver.number, "0.0.1")
+
+    def test_version_immutable(self):
+        ver = Version("0.0.1")
+        with pytest.raises(TypeError) as e:
+            ver.number = "0.0.2"
